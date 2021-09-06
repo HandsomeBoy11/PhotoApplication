8fdca1a492e20558d4e8e8c153347f8b3e7fb075
王俊 <w_wangjun259>
2021-09-06 16:01:50 +0800

    Initial commit

---
+++ app/src/main/java/com/longfor/photoapplication/PhotoMoveActivity.java
@@ -0,0 +1,120 @@
+package com.longfor.photoapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.gitonway.lee.niftymodaldialogeffects.lib.Effectstype;
+import com.gitonway.lee.niftymodaldialogeffects.lib.effects.BaseEffects;
+
+/**
+ * @author wangjun
+ * @date 2021/9/6 15:37
+ * @Des :
+ */
+public class PhotoMoveActivity extends AppCompatActivity implements PhotoMoveService.StopImg{
+
+    //创建一个ImageView用于关联布局文件中的控件
+    private ImageView show;
+    //该布尔值变量用于判断点击事件，因为这里是点击屏幕就开始相片轮播，防止用户点击多次发生紊乱，这里限制一下
+    private Boolean click = false;
+    //用于判断图片是否轮播
+    private Boolean run = true;
+    //数组，存放的是图片
+    private int[] imgs = {R.drawable.a1,R.drawable.a1,R.drawable.a1,R.drawable.a1,
+            R.drawable.a1,R.drawable.a1,R.drawable.a1,R.drawable.a1,
+            R.drawable.a1,R.drawable.a1,R.drawable.a1,R.drawable.a1,R.drawable.a1};
+    //数组，存放的是各种动画效果
+    private Effectstype[] types={Effectstype.Fadein,Effectstype.Fall,Effectstype.Fliph,Effectstype.Flipv,
+            Effectstype.Newspager,Effectstype.RotateBottom,Effectstype.RotateLeft,Effectstype.Shake,
+            Effectstype.Sidefill,Effectstype.SlideBottom,Effectstype.Slideright,Effectstype.Slidetop,Effectstype.Slideleft,
+            Effectstype.Slit};
+    //用于获取子线程传过来的值，并在UI线程做出响应
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            start(msg.arg1,4000,msg.what);
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_move);
+        show = (ImageView) findViewById(R.id.show);
+        //监听屏幕中的ImageView控件被点击，开启服务，奏起音乐，并且调用子线程，开始轮播相册。
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (click){
+                    return;
+                }
+                click = true;
+                Intent intent = new Intent(PhotoMoveActivity.this,PhotoMoveService.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startService(intent);
+                start(6,4000,6);
+                PhotoMoveService.setMusicStop(PhotoMoveActivity.this);
+                new Thread(new ImgNext()).start();
+            }
+        });
+    }
+
+    //实现Service中接口的方法，用来停止轮播。
+    @Override
+    public void stop() {
+        run = false;
+    }
+
+    //子线程，用于循环轮播相册，每次循环，将参数传给显示图片的方法。
+    public class ImgNext implements Runnable{
+
+        @Override
+        public void run() {
+            int i = -1;
+            int which = -1;
+            Message message;
+            while (i<imgs.length&&run){
+                if (which == types.length-1){
+                    which = -1;
+                }
+                if (i == imgs.length-1){
+                    i = -1;
+                }
+                try {
+                    Thread.sleep(5000);
+                    message = new Message();
+                    i++;
+                    which++;
+                    message.what = i;
+                    message.arg1 = which;
+                    handler.sendMessage(message);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (!run){
+                message = new Message();
+                message.what = 10;
+                message.arg1 = 0;
+                handler.sendMessage(message);
+            }
+        }
+    }
+
+    //将图片放到ImageView，再给ImageView附加动画的方法
+    private void start(int which,int mDuration,int img) {
+        show.setImageResource(imgs[img]);
+        BaseEffects animator = types[which].getAnimator();
+        if (mDuration != -1) {
+            animator.setDuration(Math.abs(mDuration));
+        }
+        animator.start(show);
+    }
+
+}
---
+++ app/src/main/java/com/longfor/photoapplication/PhotoMoveService.java
@@ -0,0 +1,63 @@
+package com.longfor.photoapplication;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+/**
+ * @author wangjun
+ * @date 2021/9/6 15:32
+ * @Des :
+ */
+public class PhotoMoveService extends Service {
+    public PhotoMoveService() {
+    }
+
+    private static StopImg stopImg;
+    private MediaPlayer mediaPlayer;
+    private Boolean aBoolean = false;
+
+    @Override
+    public void onCreate() {
+        //创建音乐媒体播放器，音乐文件放到res/raw文件夹中。
+        mediaPlayer = MediaPlayer.create(this,R.raw.active_second_step);
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        //开始播放音乐
+        mediaPlayer.start();
+        //监听音乐是否播放完毕，播放完了就运用回调停止Activity中的图片轮播。
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                stopImg.stop();
+            }
+        });
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    //获取Activity，将Service和Activity关联起来，为回调做准备。
+    public static void setMusicStop(Context context){
+        stopImg = (StopImg) context;
+    }
+
+    //实现一个接口，用于回调
+    public interface StopImg{
+        public void stop();
+    }
+}
